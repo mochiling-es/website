68c51c22be6af640fa6a1ff2f42f67d4aea3ee68
xavijam <xavijam@gmail.com>
2017-02-08 23:31:30 +0100

    Adding more things

--- app/_assets/js/member.js
+++ app/_assets/js/member.js
@@ -1,10 +1,11 @@
 var $ = require('jquery');
 var MemberMapView = require('./member-map-view');
-
+var Instafeed = require('instafeed.js');
 
 $(function(){
   var visitedCountries = window.visitedCountries;
   var bornLocation = window.bornLocation;
+  var instagramId = window.instagramId;
 
   if (!visitedCountries) throw new Error('visitedCountries is required');
   if (!bornLocation) throw new Error('bornLocation is required');
@@ -16,4 +17,20 @@ $(function(){
   });
 
   memberMap.render();
+
+  if (instagramId) {
+    var feed = new Instafeed({
+      get: 'user',
+      limit: 1,
+      userId: instagramId,
+      clientId: '8edddd77898d4100bfac8f4b58e54c25',
+      accessToken: '9841730.ba4c844.3ce456308101453787eb5443d358c259',
+      resolution: 'standard_resolution',
+      after: function () {
+        $('#instafeed img').addClass('pure-img');
+        console.log(arguments);
+      }
+    });
+    feed.run();
+  }
 });
