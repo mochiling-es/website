898511826b21d7627ac2464b3ba8a2218a812ee4
xavijam <xavijam@gmail.com>
2017-03-11 11:55:20 +0100

    Fixing instagram and last-experience banners

--- app/_assets/js/members.js
+++ app/_assets/js/members.js
@@ -1,7 +1,10 @@
 var $ = require('jquery');
 var DefaultMapView = require('./components/default-map-view');
+var ResponsiveHeaderHelper = require('./components/responsive-header');
 
-$(function(){
+$(function () {
+  ResponsiveHeaderHelper($('.js-canvas'));
+
   var defaultMap = new DefaultMapView({
     el: $('#js-map')
   });
