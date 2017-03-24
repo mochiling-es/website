7905c391602c9a258f49c35d4e504b63f143ecee
xavijam <xavijam@gmail.com>
2017-03-24 19:32:38 +0100

    Using local geojson

--- app/_assets/js/components/default-map-view.js
+++ app/_assets/js/components/default-map-view.js
@@ -2,7 +2,7 @@ var L = require('leaflet');
 var $ = require('jquery');
 var _ = require('underscore');
 var Backbone = require('backbone');
-var COUNTRIES_SQL = 'http://mochiling.carto.com/api/v2/sql?format=GeoJSON&q=SELECT ST_SIMPLIFY(the_geom, 0.3) as the_geom FROM world_borders';
+var CountriesJSON = require('../countries.json');
 var POLYGON_STYLE = {
   color: '#EFEFEF',
   weight: 1,
@@ -18,15 +18,14 @@ module.exports = Backbone.View.extend({
       dragging: false,
       attributionControl: false,
       scrollWheelZoom: false,
+      zoomControl: false,
       touchZoom: false,
       keyboard: false
     }).setView([29.142566, -33.925781], 3);
 
-    $.getJSON(COUNTRIES_SQL, function(data) {
-      L.geoJson(data, {
-        style: POLYGON_STYLE
-      }).addTo(map);
-    }.bind(this));
+    L.geoJson(CountriesJSON, {
+      style: POLYGON_STYLE
+    }).addTo(map);
 
     return this;
   }
