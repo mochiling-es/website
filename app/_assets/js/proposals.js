9b45a3d094f86527fc606bb2801173e2e3b8fc83
xavijam <xavijam@gmail.com>
2017-03-27 22:38:16 +0200

    Proposals working

--- app/_assets/js/proposals.js
+++ app/_assets/js/proposals.js
@@ -1,4 +1,6 @@
 var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
 var DefaultMapView = require('./components/default-map-view');
 var ResponsiveHeaderHelper = require('./components/responsive-header');
 
@@ -10,4 +12,40 @@ $(function () {
   });
 
   defaultMap.render();
+
+  var proposals = window.proposals;
+
+  var setRoute = function (proposalNumber) {
+    if (_.isNaN(proposalNumber) || proposalNumber > 3 || proposalNumber < 1) {
+      proposalNumber = 1;
+    }
+    var proposal = proposals[proposalNumber - 1];
+    
+    // On proposal click, we have to change
+    // - proposal selected
+    $('.js-navItem').removeClass('is-selected');
+    $('.js-navItem' + proposalNumber).addClass('is-selected');
+    // - desc
+    $('.js-itemDesc').html(proposal.desc);
+    // - contact link
+    var link = $('.js-itemContactUrl').attr('href').slice(0, -1);
+    $('.js-itemContactUrl').attr('href', link + proposalNumber);
+    // - sub text
+    $('.js-itemSub').html(proposal.sub);
+    // - features availables
+    $('.js-feature').addClass('is-disabled');
+    _.each(proposal.features, function (feature) {
+      $('.js-feature-' + feature).removeClass('is-disabled');
+    });
+  };
+
+    // Initiate the router
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+      "*proposal": setRoute
+    }
+  });
+
+  var router = new AppRouter();
+  Backbone.history.start();
 });
